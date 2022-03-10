55bab774e04aca31578239204a970ff9e95f41a7
Ben Hamel <benehamel@gmail.com>
2022-03-10 17:05:57 -0330

    working on hit button

--- iOS Game Project Blackjack Ben Hamel/GameViewController.swift
+++ iOS Game Project Blackjack Ben Hamel/GameViewController.swift
@@ -14,7 +14,10 @@ class GameViewController: UIViewController {
     var playerCards: [PlayingCard] = []
     var houseCards: [PlayingCard] = []
     var playerScore: Int = 0;
+    var playerCardTally: Int = 0;
     var houseScore: Int = 0;
+    var houseCardTally: Int = 0;
+    var gameIsWon = false
 //    let deck: CardDeck
 //    var playerCards: [PlayingCard]
     
@@ -28,28 +31,37 @@ class GameViewController: UIViewController {
     
     @IBAction func hitButtonPressed(_ sender: UIButton) {
         playerCards.append(deck.drawCard()!)
-        print("Player",playerCards)
-//        print("house",houseCards)
-        for card in playerCards{
-            print(card.cardValue.rawValue)
+        playerCardTally = tallyCards(tallyTheseCards: playerCards)
+        if playerCardTally > 21 {
+            print("Bust")
         }
+        print("Player Tally", playerCardTally)
+//        print("Player",playerCards)
+//        print("house",houseCards)
+//        for card in playerCards{
+//            print(card.cardValue.rawValue)
+//        }
+//        tallyCards()
+        
+//
+        
     }
     
     
     
     @IBAction func stayButtonPressed(_ sender: UIButton) {
-        let msg = "test"
+//        let msg = "test"
         
-        let alert = UIAlertController(title: "Alert", message: msg, preferredStyle: UIAlertController.Style.alert)
-        alert.addAction(UIAlertAction(title: "Close Alert", style:.destructive, handler: {(alert: UIAlertAction!) in self.resetGame()}))
-        self.present(alert, animated: true, completion: nil)
+//        let alert = UIAlertController(title: "Alert", message: msg, preferredStyle: UIAlertController.Style.alert)
+//        alert.addAction(UIAlertAction(title: "Close Alert", style:.destructive, handler: {(alert: UIAlertAction!) in self.resetGame()}))
+//        self.present(alert, animated: true, completion: nil)
+       
+        print("Player Tally", playerCardTally)
+        print("House Tally", houseCardTally)
     }
     
     
     @IBAction func startGameButtonPressed(_ sender: UIButton) {
-//        let deck = CardDeck()
-//        var playerCards: [PlayingCard] = []
-        
         deck.createDeck()
         deck.shuffleDeck()
 //        deck.drawCard()
@@ -58,11 +70,23 @@ class GameViewController: UIViewController {
         playerCards.append(deck.drawCard()!)
         houseCards.append(deck.drawCard()!)
         houseCards.append(deck.drawCard()!)
-    
- 
-        print(playerCards)
-       
         
+ 
+        
+        playerCardTally = tallyCards(tallyTheseCards: playerCards)
+        houseCardTally = tallyCards(tallyTheseCards: houseCards)
+        print("Start Game - Player Tally", playerCardTally)
+        print("Start Game - House Tally", houseCardTally)
+    }
+    
+    func tallyCards(tallyTheseCards: [PlayingCard])-> Int {
+        var tally = 0;
+        let cards = tallyTheseCards
+        for card in cards{
+            tally += card.cardValue.rawValue
+        }
+        
+        return tally
     }
     
     
